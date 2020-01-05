54e80af461955bd2b245c96589e5c7d4e7968049
RuqayyahSara <sarasadiya4@gmail.com>
2020-01-05 23:17:18 +0530

    Setup react using 'create react app'

---
+++ myapp/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Welcome from './Welcome';
+import Fun from './Fun';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World from React App</h1>
+      <Welcome/>
+      <Fun/>
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/components/Fun.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Fun(){
+    return(
+        <h1>I am Fun Component</h1>
+    )
+}
+
+export default Fun;
